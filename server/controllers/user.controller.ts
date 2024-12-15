e933393aa98ccb522a86b13092d8354eb5efdc77
davidran1 <157808421+davidran1@users.noreply.github.com>
2024-12-15 20:00:34 +0200

    Controller bug fix

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -154,4 +154,21 @@ export const loginUser = CatchAsyncError(async(req:Request , res:Response , next
   catch(error:any){
     return next (new ErrorHandler(error.message , 400));
   }
-})
+});
+
+// logout user
+export const logoutUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+
+      res.status(200).json({
+        success: true,
+        message: "התנתקת בהצלחה",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
