6af7c3bc965ca7dcd8e821d01716e5f56b1dedeb
davidran1 <157808421+davidran1@users.noreply.github.com>
2024-12-16 17:36:35 +0200

    add auth , social auth , get user info

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -3,13 +3,18 @@ import express, { Request, Response, NextFunction } from "express";
 import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
-import jwt, { Secret } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
-import { sendToken } from "../utils/jwt";
+import {
+  accessTokenOptions,
+  sendToken,
+  refreshTokenOptions,
+} from "../utils/jwt";
 import { redis } from "../utils/redis";
 import { RedisKey } from "ioredis";
+import { getUserById } from "../services/user.service";
 
 //register user
 interface IRegistrationBody {
@@ -107,20 +112,20 @@ export const activateUser = CatchAsyncError(
         return next(new ErrorHandler("קוד אימות לא תקין", 400));
       }
 
-      const {name ,email , password} = newUser.user;
-      const existUser = await userModel.findOne({email});
-      if(existUser){
+      const { name, email, password } = newUser.user;
+      const existUser = await userModel.findOne({ email });
+      if (existUser) {
         return next(new ErrorHandler("מייל כבר קיים במערכת!", 400));
       }
       const user = await userModel.create({
-        name, 
+        name,
         email,
         password,
       });
       res.status(201).json({
-        success:true,
-        message:"נרשמת בהצלחה למערכת!"
-      })
+        success: true,
+        message: "נרשמת בהצלחה למערכת!",
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
@@ -128,34 +133,34 @@ export const activateUser = CatchAsyncError(
 );
 
 //login user
-interface ILoginRequest{
-  email:string;
-  password:string;
+interface ILoginRequest {
+  email: string;
+  password: string;
 }
 
-export const loginUser = CatchAsyncError(async(req:Request , res:Response , next:NextFunction)=>{
-  try{
-    const {email,password} = req.body as ILoginRequest;
-    if(!email || !password){
-      return next (new ErrorHandler("אנא הזן מייל וסיסמא" , 400));
-    }
+export const loginUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILoginRequest;
+      if (!email || !password) {
+        return next(new ErrorHandler("אנא הזן מייל וסיסמא", 400));
+      }
 
-    const user = await userModel.findOne({email}).select("+password");
-    if(!user){
-      return next (new ErrorHandler("מייל או סיסמא אינם נכונים", 400));
-    }
+      const user = await userModel.findOne({ email }).select("+password");
+      if (!user) {
+        return next(new ErrorHandler("מייל או סיסמא אינם נכונים", 400));
+      }
 
-    const isPasswordMatch = await user.comparePassword(password);
-    if(!isPasswordMatch){
-      return next (new ErrorHandler("מייל או סיסמא אינם נכונים", 400));
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("מייל או סיסמא אינם נכונים", 400));
+      }
+      sendToken(user, 200, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
     }
-    sendToken(user,200,res);
   }
-
-  catch(error:any){
-    return next (new ErrorHandler(error.message , 400));
-  }
-});
+);
 
 // logout user
 export const logoutUser = CatchAsyncError(
@@ -163,11 +168,89 @@ export const logoutUser = CatchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
-      redis.del(req.user?._id as RedisKey);//delete from redis
+      redis.del(req.user?._id as RedisKey); //delete from redis
       res.status(200).json({
         success: true,
         message: "התנתקת בהצלחה",
       });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//update access token
+
+export const updateAccessToken = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const refresh_token = req.cookies.refresh_token as string;
+      const decoded = jwt.verify(
+        refresh_token,
+        process.env.REFRESH_TOKEN as string
+      ) as JwtPayload;
+      if (!decoded) {
+        return next(new ErrorHandler("Could not refresh token", 400));
+      }
+
+      const session = await redis.get(decoded.id as string);
+      if (!session) {
+        return next(new ErrorHandler("Could not refresh token", 400));
+      }
+
+      const user = JSON.parse(session);
+      const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN as string,
+        { expiresIn: "5m" }
+      );
+      const refreshToken = jwt.sign(
+        { id: user._id },
+        process.env.REFRESH_TOKEN as string,
+        { expiresIn: "3d" }
+      );
+      res.cookie("access_token", accessToken, accessTokenOptions);
+      res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      res.status(200).json({
+        status: "success",
+        accessToken,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//get user info
+export const getUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user?._id as string;
+      getUserById(userId, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+interface ISocialAuthBody {
+  email: string;
+  name: string;
+  avatar: string;
+}
+//social auth
+export const socialAuth = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, name, avatar } = req.body as ISocialAuthBody;
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        const newUser = await userModel.create({ email, name, avatar });
+        sendToken(newUser, 200, res);
+      } else {
+        sendToken(user, 200, res);
+      }
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,11 +1,15 @@
 import express from 'express'
-import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/user.controller';
-import { isAuthenticated } from '../middleware/auth';
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser,socialAuth,updateAccessToken } from '../controllers/user.controller';
+import { isAuthenticated , authorizeRoles } from '../middleware/auth';
 const userRouter=express.Router();
 
 userRouter.post('/registration',registrationUser);
 userRouter.post('/activate-user',activateUser);
 userRouter.post('/login',loginUser);
-userRouter.get('/logout',isAuthenticated,logoutUser);
+userRouter.get('/logout',isAuthenticated,authorizeRoles("user"),logoutUser);
+userRouter.get("/refresh",updateAccessToken);
+userRouter.get("/me",isAuthenticated,getUserInfo);
+userRouter.post('/social-auth',socialAuth)
+
 
 export default userRouter;
