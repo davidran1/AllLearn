8a6bd7b25399e9b69b82c2ccecec1df08b5aa25a
davidran1 <157808421+davidran1@users.noreply.github.com>
2024-12-17 17:36:46 +0200

    get single course , get all courses , save the course in redis(cachce) in json format to prevent calls to mongo

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -76,6 +76,63 @@ export const editCourse = CatchAsyncError(
   }
 );
 
+//Get all courses without purchasing
+export const getAllCourses = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const isCacheExist = await redis.get("allCourses");
+      if(isCacheExist){
+        const courses = JSON.parse(isCacheExist);
+        res.status(200).json({
+          success: true,
+          courses,
+        });
+      }
+      else{
+        const courses = await CourseModel.find().select(
+            "-courseData.videoUrl -courseData.questions -courseData.links"
+          );
+          await redis.set("allCourses",JSON.stringify(courses));//save the course to redis for next time coming(cache)
+          res.status(200).json({
+            success: true,
+            courses,
+          });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+//Get single course without purchasing
+export const getSingleCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId = req.params.id;
+      const isCacheExist = await redis.get(courseId);
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist);
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }
+      else{
+        const course = await CourseModel.findById(req.params.id).select(
+            "-courseData.videoUrl -courseData.questions -courseData.links"
+          );
+          redis.set(courseId , JSON.stringify(course));//save the course to redis for next time coming(cache)
+          res.status(200).json({
+            success: true,
+            course,
+          });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
 /*
 export const uploadCourse = CatchAsyncError(async(req:Request,res:Response ,next:NextFunction)=>{
     try{
