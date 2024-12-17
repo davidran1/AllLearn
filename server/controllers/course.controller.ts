53be7f71e4759323fedbc561f465025e5bdf7ef0
davidran1 <157808421+davidran1@users.noreply.github.com>
2024-12-17 16:27:21 +0200

    add course model , add create new course , add edit exist course

---
+++ server/controllers/course.controller.ts
@@ -0,0 +1,87 @@
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendMail";
+import {
+  accessTokenOptions,
+  sendToken,
+  refreshTokenOptions,
+} from "../utils/jwt";
+import { redis } from "../utils/redis";
+import { RedisKey } from "ioredis";
+import { getUserById } from "../services/user.service";
+import cloudinary from "cloudinary";
+import { createCourse } from "../services/course.service";
+import CourseModel from "../models/course.model";
+
+//upload course
+export const uploadCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      createCourse(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+//edit course
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      const courseId = req.params.id;
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+/*
+export const uploadCourse = CatchAsyncError(async(req:Request,res:Response ,next:NextFunction)=>{
+    try{
+
+   }catch(error:any) {
+    return next(new ErrorHandler(error.message,500));
+   }
+})
+*/
